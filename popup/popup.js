f8e694cedce9383165789659cdcfb6e0418bd2d6
Arisa Hirata <arisa.pirata@gmail.com>
2021-06-12 14:07:12 -0700

    Timer Feature Part 2 - implemented timer(just count up from 0)

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,15 @@
 let tasks = [];
 
+function updateTime() {
+	chrome.storage.local.get(["timer"], (res) => {
+		const time = document.getElementById("time");
+		time.textContent = res.timer;
+	});
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
 const startTimerBtn = document.getElementById("start-timer-btn");
 startTimerBtn.addEventListener("click", () => {
 	chrome.storage.local.get(["isRunning"], (res) => {
