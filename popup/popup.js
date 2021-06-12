53f1178b7f113f76f1c5cc22ff718838eebf5a2d
Arisa Hirata <arisa.pirata@gmail.com>
2021-06-12 14:34:35 -0700

    Timer Feature Part 2 - timer count down from 25minutes. also added padStart(00)

--- popup/popup.js
+++ popup/popup.js
@@ -3,7 +3,12 @@ let tasks = [];
 function updateTime() {
 	chrome.storage.local.get(["timer"], (res) => {
 		const time = document.getElementById("time");
-		time.textContent = res.timer;
+		const minutes = `${25 - Math.ceil(res.timer / 60)}`.padStart(2, "0");
+		let seconds = "00";
+		if (res.timer % 60 != 0) {
+			seconds = `${60 - res.timer % 60}`.padStart(2, "0");
+		}
+		time.textContent = `${minutes}:${seconds}`;
 	});
 }
 
